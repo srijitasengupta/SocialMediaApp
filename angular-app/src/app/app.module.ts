572778c5d911cea5a5f31e8b7ad9e3d6b52c09be
Oindrila-Chakraborty <ochakraborty937@gmail.com>
2023-05-03 23:40:11 +0530

    firebase module imported

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {AngularFireModule} from '@angular/fire/compat'
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
