a0e95907c563b3217df9af8d0f4860b2928ee806
srijitasengupta <july99srijita@gmail.com>
2023-05-04 00:58:26 +0530

    Register component added

--- angular-app/src/app/components/register/register.component.ts
+++ angular-app/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  email : string = '';
+  password : string = '';
 
+  constructor(private auth : AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+
+    if(this.email == '') {
+      alert('Please enter email');
+      return;
+    }
+
+    if(this.password == '') {
+      alert('Please enter password');
+      return;
+    }
+
+    this.auth.register(this.email,this.password);
+
+    this.email = '';
+    this.password = '';
+
+  }
 }
